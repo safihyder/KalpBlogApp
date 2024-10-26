8b0aa3ed232a8144d6bdf1b6bd9baa3fdb21b9a3
Safi Hyder <safihaider0987@gmail.com>
2024-10-26 11:29:56 +0530

    version1

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+import LogoutBtn from "./Header/LogoutBtn";
+import Select from "./Select";
+import RTE from "./RTE";
+import Container from "./container/Container";
+import Signup from "../pages/Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+
+export {Footer,Header,Logo,Button,Input,Select,RTE,Container,AuthLayout,PostCard,Login,PostForm,LogoutBtn,Signup}
